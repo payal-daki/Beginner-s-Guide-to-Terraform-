752d9b75fc3fa19c5a18d48eb2c54cf9eb8fdc81
Payal Daki <130816609+payal-daki@users.noreply.github.com>
2024-07-08 09:05:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI ID
   instance_type = "t2.micro"               # Specify your desired instance type
   subnet_id     = "subnet-12345678"        # Replace with your subnet ID
-  key_name= "aws_key_name"                 #add aws key pair
+  key_name = "aws_key_name"                 #add aws key pair
 }
